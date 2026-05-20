e5dcc3185d4820a9fb13f2f595a50c8df85b927a
Nimesh Dulal <nimeshdulal870@gmail.com>
2026-05-20 09:06:56 +0545

    auth layout setup

---
+++ src/components/AuthLayout.jsx
@@ -0,0 +1,25 @@
+import React , { useEffect , useState } from 'react'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+
+const AuthLayout = ({children , authentication = true}) => {
+
+    const navigate = useNavigate()
+    const [loader, setLoader] = useState(true)
+    const authStatus = useSelector( state => state.auth.status )
+
+    useEffect(() => {
+            
+        if ( authentication && authStatus !== authentication ) {
+            navigate('/login')
+        }
+        else if( !authentication && authStatus !== authentication ) {
+            navigate('/')
+        }
+        setLoader(false)
+    }, [authStatus , navigate , authentication])
+
+    return loader ? <h1>Loading,Please wait...</h1> : {children}
+}
+
+export default AuthLayout
