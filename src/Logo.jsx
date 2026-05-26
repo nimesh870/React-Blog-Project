89b98735945d418cf60e94a497ee9f039dc366a6
Nimesh Dulal <nimeshdulal870@gmail.com>
2026-05-26 13:28:16 +0545

    Added some images

--- src/Logo.jsx
+++ src/Logo.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Logo = ({width = '100px'}) => {
   return (
-    <div>Logo</div>
+    <div>
+      <img src="src\assets\blog.svg" alt="blog" />
+    </div>
   )
 }
 
